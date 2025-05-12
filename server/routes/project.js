894d09544a418e0558ff6ba1f1ec4034bfea1629
ZEnd <om99305@gmail.com>
2025-05-12 20:12:19 +0700

    edit protect

--- server/routes/project.js
+++ server/routes/project.js
@@ -4,10 +4,10 @@ const { protect, isAdmin } = require("../middlewares/auth");
 const upload = require('../middlewares/uploadImage');
 const { getProjects, deleteProjects, getProjectsById, postProjects, putProjects } = require("../controllers/project");
 
-router.get('/projects', protect, isAdmin, getProjects)
-router.get('/projects/:id', protect, isAdmin, getProjectsById)
+router.get('/projects', getProjects)
+router.get('/projects/:id', getProjectsById)
 router.post('/projects', protect, isAdmin, upload.single('image'), postProjects)
-router.put('/projects/:id', protect, isAdmin,upload.single('image'), putProjects)
+router.put('/projects/:id', protect, isAdmin, upload.single('image'), putProjects)
 router.delete('/projects/:id', protect, isAdmin, deleteProjects)
 
 
